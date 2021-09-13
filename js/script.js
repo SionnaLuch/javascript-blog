f29298a242641950860c69e420a71d237646c7e3
Sionna Poleszuk <sionna.pol@gmail.com>
2021-09-13 16:08:59 +0200

    Dokończenie handlera eventu

--- js/script.js
+++ js/script.js
@@ -2,31 +2,41 @@
   //const links = document.querySelectorAll('.titles a');
   //console.log('links:', links);
 //});
-const titleClickHandler = function(event){
+const titleClickHandler = function (event) {
+  event.preventDefault();
   const clickedElement = this;
-  console.log('Link was clicked!');
+  // console.log('Link was clicked!');
+
   /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
-  for(let activeLink of activeLinks){
+  for (let activeLink of activeLinks) {
     activeLink.classList.remove('active');
   }
-  /*[IN PROGRESS] add class 'active' to the clicked link */
+
+  /* [DONE] add class 'active' to the clicked link */
   clickedElement.classList.add('active');
-  console.log('clickedElement', clickedElement);
+  // console.log('clickedElement:', clickedElement);
+
   /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.post.active');
 
-  for(let activeArticle of activeArticles){
+  for (let activeArticle of activeArticles) {
     activeArticle.classList.remove('active');
   }
-  /* get 'href' attribute from the clicked link */
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  /* [DONE] get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href');
+  // console.log(articleSelector);
 
-  /* add class 'active' to the correct article */
-}
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle = document.querySelector(articleSelector);
+  // console.log(targetArticle);
 
+  /* [DONE] add class 'active' to the correct article */
+  const clickedArticle = document.querySelector(articleSelector);
+  clickedArticle.classList.add('active');
+};
 const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
